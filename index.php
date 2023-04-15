90dd834e22a477b303788829f364fb1eb9b51288
mass420 <masuk27roll@gmail.com>
2023-04-15 15:24:38 +0530

    modified

--- index.php
+++ index.php
@@ -1,6 +1,64 @@
-<?php include './components/header.php' ?>
-<?php include './components/nav.php' ?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Alumni Connect</title>
 
+    <?php require('user/subpage/links.php'); ?>
+    <link rel="stylesheet" href="./style.css">
 
-<?php include './components/footer.php' ?>
+</head>
+
+<body>
+
+    <?php require('./user/subpage/header.php'); ?>
+    <!-- Main Content Area -->
+    <div class="container my-5 d-grid gap-5">
+        <div class="p-5 border">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi.
+            </p>
+        </div>
+        <div class="p-5 border">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi.
+            </p>
+        </div>
+        <div class="p-5 border">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi.
+            </p>
+        </div>
+        <div class="p-5 border">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi.
+            </p>
+        </div>
+        <div class="p-5 border">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+                natus iusto fugit id saepe neque rerum magni laudantium accusantium
+                dolorem numquam quasi. df
+            </p>
+        </div>
+    </div>
+
+    <?php require('./user/subpage/footer.php'); ?>
+</body>
+
+</html>
---
+++ user/subpage/header.php
@@ -0,0 +1,51 @@
+<!-- Navbar  -->
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Karimganj College</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <!-- <div class="mx-auto"> </div> -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">PROGRAM & EVENTS</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        ALUMNI PROFILE
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Sign Up</a></li>
+                        <li><a class="dropdown-item" href="#">Login</a></li>
+
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        DEPERTMENTS
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">CS ALUMNI</a></li>
+                        <li><a class="dropdown-item" href="#">STATISTICS ALUMNI</a></li>
+
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Banner Image  -->
+<div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+    <div class="content text-center">
+        <h1 class="text-white">Alumni Connect</h1>
+    </div>
+</div>
