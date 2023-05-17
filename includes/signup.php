e3ac5f44d0d5ae624eb255b8a045421b6d6e71d8
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2023-05-17 13:44:56 +0530

    fixed bugs ig

--- includes/signup.php
+++ includes/signup.php
@@ -1,99 +1,90 @@
 <?php
 
-	/* validate firstname */
-	if(empty($_POST["firstname"])) {
-		$info["errors"]["firstname"] = "A first name is required";
-	} else if(!preg_match("/^[\p{L}]+$/", $_POST["firstname"])) {
-		$info["errors"]["firstname"] = "First name cant have special characters or spaces and numbers";
-	}
+$info = array("errors" => array());
 
-	/* validate lastname */
-	if(empty($_POST["lastname"])) {
-		$info["errors"]["lastname"] = "A last name is required";
-	} else if(!preg_match("/^[\p{L}]+$/", $_POST["lastname"])) {
-		$info["errors"]["lastname"] = "Last name cant have special characters or spaces and numbers";
-	}
+/* validate firstname */
+if(empty($_POST["firstname"])) {
+    $info["errors"]["firstname"] = "A first name is required";
+} else if(!preg_match("/^[\p{L}]+$/u", $_POST["firstname"])) {
+    $info["errors"]["firstname"] = "First name can't have special characters, spaces, or numbers";
+}
 
-	/* validate email */
-	if(empty($_POST["email"])) {
-		$info["errors"]["email"] = "An email is required";
-	} else if(!filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)) {
-		$info["errors"]["email"] = "Email is not valid";
-	}
+/* validate lastname */
+if(empty($_POST["lastname"])) {
+    $info["errors"]["lastname"] = "A last name is required";
+} else if(!preg_match("/^[\p{L}]+$/u", $_POST["lastname"])) {
+    $info["errors"]["lastname"] = "Last name can't have special characters, spaces, or numbers";
+}
 
-	//validate gender
-	$genders = ["Male","Female", "Others"];
-	if(empty($_POST["gender"])) {
-		$info["errors"]["gender"] = "A gender is required";
-	} else if(!in_array($_POST["gender"], $genders)) {
-		$info["errors"]["gender"] = "Gender is not valid";
-	}
+/* validate email */
+if(empty($_POST["email"])) {
+    $info["errors"]["email"] = "An email is required";
+} else if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    $info["errors"]["email"] = "Email is not valid";
+}
 
-	/* validate password */
-	if(empty($_POST["password"])) {
-		$info["errors"]["password"] = "A password is required";
-	} else if($_POST["password"] !== $_POST["retype_password"]) {
-		$info["errors"]["password"] = "Passwords dont match";
-	} else if(strlen($_POST["password"]) < 8) {
-		$info["errors"]["password"] = "Password must be at least 8 characters long";
-	}
+// validate gender
+$genders = array("Male", "Female", "Others");
+if(empty($_POST["gender"])) {
+    $info["errors"]["gender"] = "A gender is required";
+} else if(!in_array($_POST["gender"], $genders)) {
+    $info["errors"]["gender"] = "Gender is not valid";
+}
 
-	/* validate batchyr */
-	if(empty($_POST["batchyr"])) {
-		$info["errors"]["batchyr"] = "A batchyr is required";
-	} else if(!filter_var($_POST["batchyr"], $_POST["batchyr"] )) {
-		$info["errors"]["batchyr"] = "batchyr is not valid";
-	}
+/* validate password */
+if(empty($_POST["password"])) {
+    $info["errors"]["password"] = "A password is required";
+} else if($_POST["password"] !== $_POST["retype_password"]) {
+    $info["errors"]["password"] = "Passwords don't match";
+} else if(strlen($_POST["password"]) < 8) {
+    $info["errors"]["password"] = "Password must be at least 8 characters long";
+}
 
-	/* validate occupation */
-	if(empty($_POST["occupation"])) {
-		$info["errors"]["occupation"] = "An occupation is required";
-	} else if(!filter_var($_POST["occupation"], $_POST["occupation"] )) {
-		$info["errors"]["occupation"] = "occupation is not valid";
-	}
+/* validate batchyr */
+if(empty($_POST["batchyr"])) {
+    $info["errors"]["batchyr"] = "A batchyr is required";
+} else if(!filter_var($_POST["batchyr"], FILTER_VALIDATE_INT)) {
+    $info["errors"]["batchyr"] = "Batch year is not valid";
+}
 
-	/* validate validfile */
-	if(!empty($_FILES['validfile']['name']))
-	{
-		$folder = "documents/";
-		if(!file_exists($folder))
-		{
-			mkdir($folder, 0777, true);
-			file_put_contents($folder.'index.html', 'Access denied');
-		}
+/* validate occupation */
+if(empty($_POST["occupation"])) {
+    $info["errors"]["occupation"] = "An occupation is required";
+}
 
-		$allowed = ['image/jpeg','image/png', 'document/pdf',];
-		if(in_array($_FILES['validfile']['type'], $allowed))
-		{
-			$validfile = $folder . time() . $_FILES['validfile']['name'];
-			move_uploaded_file($_FILES['validfile']['tmp_name'], $validfile);
+/* validate validfile */
+if(!empty($_FILES['validfile']['name'])) {
+    $folder = "documents/";
+    if(!file_exists($folder)) {
+        mkdir($folder, 0777, true);
+        file_put_contents($folder.'index.html', 'Access denied');
+    }
 
-		}else{
-			$info['errors']['validfile'] = "Only validfiles of this type allowed: ".implode(", ", $allowed);
-		}
-	}
+    $allowed = array('image/jpeg', 'image/png', 'application/pdf');
+    if(in_array($_FILES['validfile']['type'], $allowed)) {
+        $validfile = $folder . time() . $_FILES['validfile']['name'];
+        move_uploaded_file($_FILES['validfile']['tmp_name'], $validfile);
+    } else {
+        $info['errors']['validfile'] = "Only valid files of the following types are allowed: ".implode(", ", $allowed);
+    }
+}
 
+if(empty($info["errors"])) {
+    // save to database or perform further processing
+    $arr = array();
+    $arr["firstname"] = $_POST["firstname"];
+    $arr["lastname"] = $_POST["lastname"];
+    $arr["email"] = $_POST["email"];
+    $arr["batchyr"] = $_POST["batchyr"];
+    $arr["occupation"] = $_POST["occupation"];
+    $arr["validfile"] = isset($validfile) ? $validfile : '';
+    $arr["gender"] = $_POST["gender"];
+    $arr["password"] = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $arr["date"] = date("Y-m-d H:i:s");
 
-	if(empty($info["errors"])) {
-		//save to database
-		$arr = [];
-		$arr["firstname"] 	= $_POST["firstname"];
-		$arr["lastname"] 	= $_POST["lastname"];
-		$arr["email"] 		= $_POST["email"];
-		$arr["batchyr"] 		= $_POST["batchyr"];
-		$arr["occupation"] 		= $_POST["occupation"];
-		$arr["validfile"] 		= $_POST["validfile"];
-		$image_query = "";
-		if(!empty($image))
-		{
-			$arr['image'] = $image;
-			$image_query = ",image = :image";
-		}
-		$arr["gender"] 		= $_POST["gender"];
-		$arr["password"] 	= password_hash($_POST["password"], PASSWORD_DEFAULT);
-		$arr["date"] 		= date("Y-m-d H:i:s");
+    // Assuming you have a database connection and a query execution function
+    db_query("INSERT INTO users (firstname, lastname, gender, email, batchyr, occupation, validfile, password, date) VALUES (:firstname, :lastname, :gender, :email, :batchyr, :occupation, :validfile, :password, :date)", $arr);
 
-		db_query("INSERT INTO users (firstname, lastname, gender, email, batchyr, occupation, validfile password, date) values (:firstname, :lastname, :gender, :email, :batchyr, :password, :date)",$arr);
-
-		$info["success"] 	= true;
-	}
+    $info["success"] = true;
+}
+}
