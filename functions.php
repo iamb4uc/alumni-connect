124249530c80667b08a35cf700f07b6db9138088
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2023-05-18 21:08:52 +0530

    little mod to the functions

--- functions.php
+++ functions.php
@@ -2,27 +2,18 @@
 
 session_start();
 
-function db_query(string $query, array $data = array())
-{
-	$string = "mysql:hostname=localhost;dbname=final_alumni";
+function db_query(string $query, array $data = array()) {
+	$string = "mysql:host=localhost;dbname=final_alumni";
 	$con = new PDO($string, 'root', '');
 
 	$stm = $con->prepare($query);
 	$check = $stm->execute($data);
+  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	if ($check) {
-		$res = $stm->fetchAll(PDO::FETCH_ASSOC);
-		if (is_array($res) && !empty($res)) {
-			return $res;
-		}
-	}
-
-	return false;
+	return $check;
 }
 
-function is_logged_in(): bool
-{
-
+function is_logged_in(): bool {
 	if (!empty($_SESSION['PROFILE'])) {
 		return true;
 	}
@@ -30,19 +21,16 @@ function is_logged_in(): bool
 	return false;
 }
 
-function redirect($path): void
-{
+function redirect($path): void {
 	header("Location: $path");
 	die;
 }
 
-function esc(string $str): string
-{
+function esc(string $str): string {
 	return htmlspecialchars($str);
 }
 
-function get_image($path = ''): string
-{
+function get_image($path = ''): string {
 	if (file_exists($path)) {
 		return $path;
 	}
@@ -50,8 +38,7 @@ function get_image($path = ''): string
 	return './images/no-image.jpg';
 }
 
-function user(string $key = '')
-{
+function user(string $key = '') {
 	if (is_logged_in()) {
 		if (!empty($_SESSION['PROFILE'][$key])) {
 			return $_SESSION['PROFILE'][$key];
