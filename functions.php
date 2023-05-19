9b39986eea3a564f051325d8cd631cf0847fa072
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2023-05-19 22:29:35 +0530

    fixed the login form using the new db_config
    The db_query() function returns a boolean value because of this line:
    
     = ->execute();

--- functions.php
+++ functions.php
@@ -1,17 +1,49 @@
+
+/*         THIS FILE CONTAINS ALL THE FUNCTIONS           */
+/*         REQUIRED BY THE CLIENT SIDE OF THE             */
+/*         PROJECT AND ALL IS RESPONSIBLE FOR             */
+/*         CARRYING OUT THE MOTHERFUCKING QUERIES         */
+/*         FOR THE USES AS WELL AS OTHER ASSOCIATIVE      */
+/*         FILES. R AMI PROJECT KORRAM NA RE BHAI         */
+/*                                                        */
+/*               ---- SWAPNIL                             */
+/*                                                        */
+
 <?php
 
 session_start();
 
+
+
+/* ---------------------- OLD SHIT THAT IS SUPPOSED TO BREAK THE LOGIN ---------------------- */
+/* function db_query(string $query, array $data = array()) { */
+/* 	$string = "mysql:host=localhost;dbname=final_alumni"; */
+/* 	$con = new PDO($string, 'root', ''); */
+/**/
+/* 	$stm = $con->prepare($query); */
+/* 	$check = $stm->execute($data); */
+/*   $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); */
+/**/
+/* 	return $check; */
+/* } */
+/* ------------------------------------------------------------------------------------------ */
+
+
+
+
 function db_query(string $query, array $data = array()) {
-	$string = "mysql:host=localhost;dbname=final_alumni";
-	$con = new PDO($string, 'root', '');
+    $string = "mysql:host=localhost;dbname=final_alumni";
+    $con = new PDO($string, 'root', '');
 
-	$stm = $con->prepare($query);
-	$check = $stm->execute($data);
-  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stm = $con->prepare($query);
+    $stm->execute($data);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	return $check;
+    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+    return $result;
 }
+
 
 function is_logged_in(): bool {
 	if (!empty($_SESSION['PROFILE'])) {
