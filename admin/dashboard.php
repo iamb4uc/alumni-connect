242c08d60714d1f7a0cbdb904484c79d955da015
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2023-05-19 07:09:42 +0530

    wrote little php cards that show total users and admin

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,5 +1,5 @@
 <?php
-require('subpage/essential.php');
+require 'subpage/essential.php' ;
 adminLogin(); #For checking if user is login user or unauthorized  user
 ?>
 <!DOCTYPE html>
@@ -21,10 +21,23 @@ adminLogin(); #For checking if user is login user or unauthorized  user
         <div class="row">
             <div class="col-lg-10 ms-auto p-4 overflow-hidden">
               <!-- Document Body -->
-              <!-- BOX 1 -->
-                <!-- Total user -->
-              <!-- BOX 2 -->
-                <!-- Total verified user -->
+              <div class="row">
+                <div class="col-sm-4">
+                  <div class="card text-white bg-primary mb-4 text-center" style="max-width: 28rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">TOTAL USERS</h5>
+                      <p class="card-text"><?php require 'subpage/db_config.php'; getTotalusers(); ?></p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="card text-white bg-secondary mb-4 text-center" style="max-width: 28rem;">
+                    <div class="card-body">
+                      <h5 class="card-title">TOTAL ADMINS</h5>
+                      <p class="card-text"><?php getTotaladmins(); ?></p>
+                    </div>
+                </div>
+              </div>
             </div>
         </div>
     </div>
