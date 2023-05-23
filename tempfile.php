16d210f7dada58c0ecc32cb991d554c2c5ba9f71
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2023-05-23 15:40:26 +0530

    tempfile for profile edit.php

---
+++ tempfile.php
@@ -0,0 +1,27 @@
+					</table>
+					<table class="table table-striped">
+						<tr>
+							<th colspan="2">Socials</th>
+						</tr>
+						<tr>
+							<th>Facebook</th>
+							<td>
+								<input value="<?= $row['facebook'] ?>" type="text" class="form-control" name="facebook" placeholder="https://facebook.com/yourProfile">
+								<div><small class="js-error js-error-link text-danger"></small></div>
+							</td>
+						</tr>
+						<tr>
+							<th>Twitter</th>
+							<td>
+								<input value="<?= $row['twitter'] ?>" type="text" class="form-control" name="twitter" placeholder="https://twitter.com/yourProfile">
+								<div><small class="js-error js-error-link text-danger"></small></div>
+							</td>
+						</tr>
+						<tr>
+							<th>linkedin</th>
+							<td>
+								<input value="<?= $row['linkedin'] ?>" type="text" class="form-control" name="linkedin" placeholder="https://linkedin.com/yourProfile">
+								<div><small class="js-error js-error-link text-danger"></small></div>
+							</td>
+						</tr>
+					</table>
