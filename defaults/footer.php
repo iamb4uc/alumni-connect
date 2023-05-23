352dbfa85bd464e99eb5403549b941a427ec78d3
iamb4uc <103022474+iamb4uc@users.noreply.github.com>
2023-05-23 15:35:52 +0530

    updated navigation for footer

--- defaults/footer.php
+++ defaults/footer.php
@@ -26,7 +26,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <p class="mb-0 text-white">&#127279; Copyleft 2023 no right reserved | Designed by Team A</p>
+                    <p class="mb-0 text-white">&#127279; Copyleft 2023 no right reserved | Designed by Group A <br> <em>(Swapnil Bhowmik, Masuk Mazarbhuya, Moinak Shome, Priangshu Debnath, Lona)<em></p>
                 </div>
                 <div class="col-md-6">
                     <div class="social-icons">
