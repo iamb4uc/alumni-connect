9b548b5a525ae7cce5a4e1800e7add0e2b91edd4
mass420 <masuk27roll@gmail.com>
2023-05-28 17:29:48 +0530

    admin css change

--- admin/content_dash.php
+++ admin/content_dash.php
@@ -1,5 +1,5 @@
 <?php
-require 'subpage/essential.php' ;
+require 'subpage/essential.php';
 
 require 'subpage/db_config.php';
 adminLogin();
@@ -8,33 +8,41 @@ adminLogin();
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Content Dashboard</title>
-    <?php require('subpage/links.php');
-    ?>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Content Dashboard</title>
+  <style>
+    .custom-css {
+      font-family: "Sora", sans-serif;
+      line-height: 1.7;
+      color: var(--dark);
+      background: linear-gradient(-45deg, #EE7252, #E74C7E, #23A6D5, #22D5AB);
+    }
+  </style>
+  <?php require('subpage/links.php');
+  ?>
 </head>
 
-<body class="bg-light">
+<body class="custom-css">
 
-    <?php require('subpage/header.php'); ?>
-    <div class="container-fluid" id="main-content">
-      <div class="row">
-        <div class="col-lg-10 ms-auto p-4 overflow-hidden">
-          <!-- Document Body -->
-              <div class="table">
-                <h1>User Table</h1>
-                <?php dispContent(); ?>
-              </div>
-
-
-          </div>
+  <?php require('subpage/header.php'); ?>
+  <div class="container-fluid" id="main-content">
+    <div class="row">
+      <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+        <!-- Document Body -->
+        <div class="table">
+          <h1>User Table</h1>
+          <?php dispContent(); ?>
         </div>
+
+
       </div>
     </div>
+  </div>
+  </div>
 
-    <?php require('subpage/script.php'); ?>
+  <?php require('subpage/script.php'); ?>
 </body>
 
 </html>
--- admin/donation_dash.php
+++ admin/donation_dash.php
@@ -1,58 +1,67 @@
 <?php
-require 'subpage/essential.php' ;
+require 'subpage/essential.php';
 adminLogin(); #For checking if user is login user or unauthorized  user
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel - Dashboard</title>
-    <?php require('subpage/links.php');
-    ?>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin Panel - Dashboard</title>
+  <style>
+    .custom-css {
+      font-family: "Sora", sans-serif;
+      line-height: 1.7;
+      color: var(--dark);
+      background: linear-gradient(-45deg, #EE7252, #E74C7E, #23A6D5, #22D5AB);
+    }
+  </style>
+  <?php require('subpage/links.php');
+  ?>
 </head>
 
-<body class="bg-light">
+<body class="custom-css">
 
-    <?php require('subpage/header.php'); ?>
+  <?php require('subpage/header.php'); ?>
+  <div class="row">
+    <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+      <!-- Document Body -->
       <div class="row">
-        <div class="col-lg-10 ms-auto p-4 overflow-hidden">
-          <!-- Document Body -->
-          <div class="row">
-            <div class="col-sm-4">
-              <div class="card text-white bg-primary mb-4 text-center" style="max-width: 28rem;">
-                <div class="card-body">
-                  <h5 class="card-title">TOTAL Donations</h5>
-                  <p class="card-text"><?php require 'subpage/db_config.php'; getTotaldonations(); ?></p>
-                </div>
-              </div>
+        <div class="col-sm-4">
+          <div class="card text-white bg-primary mb-4 text-center" style="max-width: 28rem;">
+            <div class="card-body">
+              <h5 class="card-title">TOTAL Donations</h5>
+              <p class="card-text"><?php require 'subpage/db_config.php';
+                                    getTotaldonations(); ?></p>
             </div>
           </div>
         </div>
       </div>
-    <div class="container-fluid" id="main-content">
-        <div class="col-lg-10 ms-auto p-4 overflow-hidden">
-          <div class="row">
-              <div class="table">
-                <h1>User Table</h1>
-                <?php donationtab(); ?>
-            <form method='POST'>
+    </div>
+  </div>
+  <div class="container-fluid" id="main-content">
+    <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+      <div class="row">
+        <div class="table">
+          <h1>User Table</h1>
+          <?php donationtab(); ?>
+          <form method='POST'>
             <input type='submit' name='more' class="btn btn-secondary" value='Show More'>
             <input type='submit' name='less' class="btn btn-primary" value='Show Less'>
-            </form>
-              </div>
-          </div>
-        </div>
+          </form>
         </div>
       </div>
     </div>
-    </div>
-      </div>
-    </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  </div>
+  </div>
 
-    <?php require('subpage/script.php'); ?>
+  <?php require('subpage/script.php'); ?>
 </body>
 
 </html>
